0c23e25ec37633495c69df9566aa6cba4a17f922
Ujwol1086 <69844842+Ujwol1086@users.noreply.github.com>
2023-05-17 14:10:30 +0545

    Add files via upload

---
+++ Components/TodoData.js
@@ -0,0 +1,24 @@
+const TodoData = [
+    {
+        id:"1",
+        text:"Brush my Teeth",
+        completed: true
+    },
+    {
+        id:"2",
+        text:"Exercise",
+        completed: true
+    },
+    {
+        id:"3",
+        text:"Take the trash out",
+        completed: false
+    },
+    {
+        id:"4",
+        text:"Learn Web Development",
+        completed: false
+    }
+]
+
+export default TodoData
